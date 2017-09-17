0007ea115919c64a8842cdb07612e752adf0ef6b
Natsu028 <orangemikan.028@gmail.com>
2017-09-17 23:13:26 +0900

    教科書14

---
+++ app/src/main/java/com/example/kodakanatsumi/memopad/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.kodakanatsumi.memopad;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void memo(View v){
+
+        Intent intent = new Intent(this, MemoActivity.class);
+        startActivity(intent);
+    }
+}
